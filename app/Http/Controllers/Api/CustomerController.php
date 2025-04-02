62125529f99654c45f3d9ab3b42b09c60748217f
Long-Soeng168 <generalpurpose167@gmail.com>
2025-04-02 12:06:20 +0700

    update

--- app/Http/Controllers/Api/CustomerController.php
+++ app/Http/Controllers/Api/CustomerController.php
@@ -32,6 +32,7 @@ class CustomerController extends Controller
      */
     public function store(Request $request)
     {
+        return $request->all();
         $validated = $request->validate([
             'newCustomerName' => 'required|string|max:255',
             'newCustomerPhone' => 'nullable|string|max:20',
